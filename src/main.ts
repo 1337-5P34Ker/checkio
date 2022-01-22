fcdb0c31d877bd3773f0d93b42dbfcbfe1d9bd78
Rocco Hundertmark <rocco.tschirner@gmail.com>
2022-01-23 00:14:58 +0100

    Beginning Zeros

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import assert from "assert";
 
-function splitPairs(text: string): string[] {
-    // your code here
-    if(text.length > 0 && text.length % 2 !== 0) text += '_';
-    const array = text.split('');
-    const result: string[] = [];
-    while(array.length> 0){
-        result.push(array.splice(0,2).join(''));
+function beginningZeros(text: string): number {
+    let array = text.split('');
+    let zeros = 0;
+    while(array[0]=='0'){
+        zeros++;
+        array.shift();
     }
-    return result;
+    return zeros;
 }
 
 console.log('Example:');
-console.log(splitPairs('abcd'));
+console.log(beginningZeros('100'));
 
 // These "asserts" are used for self-checking
-assert.deepEqual(splitPairs('abcd'), ['ab', 'cd']);
-assert.deepEqual(splitPairs('abc'), ['ab', 'c_']);
-assert.deepEqual(splitPairs('abcdf'), ['ab', 'cd', 'f_']);
-assert.deepEqual(splitPairs('a'), ['a_']);
-assert.deepEqual(splitPairs(''), []);
+assert.equal(beginningZeros('100'), 0);
+assert.equal(beginningZeros('001'), 2);
+assert.equal(beginningZeros('100100'), 0);
+assert.equal(beginningZeros('001001'), 2);
+assert.equal(beginningZeros('012345679'), 1);
+assert.equal(beginningZeros('0000'), 4);
 
 console.log("Coding complete? Click 'Check' to earn cool rewards!");
