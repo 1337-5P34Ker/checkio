56572c18801690672ea68fe471d4815ae15d5b6f
Rocco Hundertmark <rocco.tschirner@gmail.com>
2022-01-22 22:29:54 +0100

    Multiply (Intro)

---
+++ src/main.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+function removeAllBefore(values, b) {
+    // your code here
+    return [];
+}
+console.log('Example:');
+console.log(removeAllBefore([1, 2, 3, 4, 5], 3));
+// These "asserts" are used for self-checking
+// assert.deepEqual(removeAllBefore([1, 2, 3, 4, 5], 3), [3, 4, 5]);
+// assert.deepEqual(removeAllBefore([1, 1, 2, 2, 3, 3], 2), [2, 2, 3, 3]);
+// assert.deepEqual(removeAllBefore([1, 1, 2, 4, 2, 3, 4], 2), [2, 4, 2, 3, 4]);
+// assert.deepEqual(removeAllBefore([1, 1, 5, 6, 7], 2), [1, 1, 5, 6, 7]);
+// assert.deepEqual(removeAllBefore([], 0), []);
+// assert.deepEqual(removeAllBefore([7, 7, 7, 7, 7, 7, 7, 7, 7], 7), [7, 7, 7, 7, 7, 7, 7, 7, 7]);
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
