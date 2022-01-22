3f3b66828ee8ea4b67342d3d8d3ab2ecaba7b9df
Rocco Hundertmark <rocco.tschirner@gmail.com>
2022-01-23 00:42:50 +0100

    Nearest Value

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import assert from "assert";
 
-function beginningZeros(text: string): number {
-    let array = text.split('');
-    let zeros = 0;
-    while(array[0]=='0'){
-        zeros++;
-        array.shift();
-    }
-    return zeros;
+function nearestValue(values: number[], search: number): number {
+    // your code here
+
+    let bestMatch = 1000000;
+    values.sort((a, b) => a - b).forEach(v => {
+        if (Math.abs(v - search) < Math.abs(bestMatch - search)) {
+            bestMatch = v;
+        }
+    })
+    return bestMatch;
 }
 
 console.log('Example:');
-console.log(beginningZeros('100'));
+console.log(nearestValue([4, 7, 10, 11, 12, 17], 9));
 
 // These "asserts" are used for self-checking
-assert.equal(beginningZeros('100'), 0);
-assert.equal(beginningZeros('001'), 2);
-assert.equal(beginningZeros('100100'), 0);
-assert.equal(beginningZeros('001001'), 2);
-assert.equal(beginningZeros('012345679'), 1);
-assert.equal(beginningZeros('0000'), 4);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 9), 10);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 8), 7);
+assert.equal(nearestValue([4, 8, 10, 11, 12, 17], 9), 8);
+assert.equal(nearestValue([4, 9, 10, 11, 12, 17], 9), 9);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 0), 4);
+assert.equal(nearestValue([4, 7, 10, 11, 12, 17], 100), 17);
+assert.equal(nearestValue([5, 10, 8, 12, 89, 100], 7), 8);
+assert.equal(nearestValue([-1, 2, 3], 0), -1);
 
 console.log("Coding complete? Click 'Check' to earn cool rewards!");
