07fbff64cc3c692cb5ffb41a427885c2524edd80
Rocco Hundertmark <rocco.tschirner@gmail.com>
2022-01-26 23:07:03 +0100

    Non-unique Elements

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,24 @@
 import assert from "assert";
 
 /*
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements 
+in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). 
+When solving this task, do not change the order of the list. 
 
-You are given a string and two markers (the initial and final). You have to find a substring enclosed between these two markers. 
-But there are a few important conditions:
+Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
 
-The initial and final markers are always different.
-If there is no initial marker, then the first character should be considered the beginning of a string.
-If there is no final marker, then the last character should be considered the ending of a string.
-If the initial and final markers are missing then simply return the whole string.
-If the final marker comes before the initial marker, then return an empty string.
-Input: Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
-
-Output: A string.
 */
 
 
-
-
-function betweenMarkers(text: string, begin: string, end: string): string {
-    const indexBegin = text.indexOf(begin);
-    const indexEnd = text.indexOf(end);
-
-    if (indexBegin == -1 && indexEnd == -1) {
-        return text;
-    }
-
-    if (indexBegin == -1 && indexEnd != -1) {
-        return text.substring(0, indexEnd);
-    }
-
-    if (indexBegin != -1 && indexEnd == -1) {
-        return text.substring(indexBegin + begin.length);
-    }
-
-    if (indexBegin != -1 && indexEnd != -1 && indexBegin < indexEnd) {
-        return text.substring(indexBegin + begin.length, indexEnd);
-    } else {
-        return '';
-    }
-    return '';
+function nonUniqueElements(data: number[]): number[] {
+    return data.filter(d=> data.lastIndexOf(d) != data.indexOf(d));
 }
 
 console.log('Example:')
-console.log(betweenMarkers('What is >apple<', '>', '<'), 'apple')
+console.log(nonUniqueElements([1, 2, 3, 1, 3]))
 
-assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple')
-assert.equal(betweenMarkers("<head><title>My new site</title></head>", "<title>", "</title>"), 'My new site')
-assert.equal(betweenMarkers('No[/b] hi', '[b]', '[/b]'), 'No')
-assert.equal(betweenMarkers('No [b]hi', '[b]', '[/b]'), 'hi')
-assert.equal(betweenMarkers('No hi', '[b]', '[/b]'), 'No hi')
-assert.equal(betweenMarkers('No <hi>', '>', '<'), '')
+assert.deepEqual(nonUniqueElements([1, 2, 3, 1, 3]), [1, 3, 1, 3]);
+assert.deepEqual(nonUniqueElements([1, 2, 3, 4, 5]), []);
+assert.deepEqual(nonUniqueElements([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5]);
+assert.deepEqual(nonUniqueElements([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9]);
 console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
