8efa3f4cb113b5eafdb1aa6237a4bd23d2a4a985
Rocco Hundertmark <rocco.tschirner@gmail.com>
2022-01-26 14:03:36 +0100

    First Word

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,21 @@
 import assert from "assert";
 
-function threeWords(text: string): boolean {
+function firstWord(text: string): string {
+    text = text.trim().replace(',','').replace(/\./g, ' ').trim();
 
     let words = text.split(' ');
-
-    let wordsCount = 0;
-    let already3Found = false;
-
-    words.forEach(word => {
-        if(isNaN(Number(word))){
-            wordsCount++
-            if(wordsCount > 2) {
-                already3Found = true;
-            }
-        } else {
-            wordsCount = 0
-        }
-    })
-    return already3Found;
+    return words[0] ;
 }
 
-console.log('Example:')
-console.log(threeWords("Hello World hello"))
 
-assert.equal(threeWords("Hello World hello"), true);
-assert.equal(threeWords("He is 123 man"), false);
-assert.equal(threeWords("1 2 3 4"), false);
-assert.equal(threeWords("bla bla bla bla"), true);
-assert.equal(threeWords("Hi"), false);
-console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+console.log('Example:')
+console.log(firstWord("Hello world"))
+
+// These "asserts" using for self-checking and not for auto-testing
+assert.equal(firstWord("Hello world"), "Hello")
+assert.equal(firstWord(" a word "), "a")
+assert.equal(firstWord("don't touch it"), "don't")
+assert.equal(firstWord("greetings, friends"), "greetings")
+assert.equal(firstWord("... and so on ..."), "and")
+assert.equal(firstWord("hi"), "hi")
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
